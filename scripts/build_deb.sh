9af5debcd771e1ca40b2094adf1b129ec5534554
Dusan Malusev <dusan.malusev@scylladb.dev>
2024-12-23 12:12:10 +0100

    feature(packaging): debian and rpm packaging
    Signed-off-by: Dusan Malusev <dusan.malusev@scylladb.dev>

--- scripts/build_deb.sh
+++ scripts/build_deb.sh
@@ -1,14 +1,15 @@
 #!/bin/bash -e
 
 . /etc/os-release
+
 print_usage() {
-    echo "build_deb.sh --reloc-pkg build/scylla-tools-package.tar.gz"
+    echo "build_deb.sh --reloc-pkg build/cassandra-stress.tar.gz"
     echo "  --reloc-pkg specify relocatable package path"
     echo "  --builddir specify Debian package build path"
     exit 1
 }
 
-RELOC_PKG=build/scylla-tools-package.tar.gz
+RELOC_PKG=build/cassandra-stress.tar.gz
 BUILDDIR=build/debian
 while [ $# -gt 0 ]; do
     case "$1" in
@@ -26,16 +27,14 @@ while [ $# -gt 0 ]; do
     esac
 done
 
-RELOC_PKG=$(readlink -f $RELOC_PKG)
-rm -rf "$BUILDDIR"/scylla-package "$BUILDDIR"/scylla-package.orig "$BUILDDIR"/debian
+RELOC_PKG=$(readlink -f "$RELOC_PKG")
+rm -rf "$BUILDDIR/scylla-package" "$BUILDDIR/scylla-package.orig" "$BUILDDIR/debian"
 mkdir -p "$BUILDDIR"/scylla-package
-tar -C "$BUILDDIR"/scylla-package -xpf $RELOC_PKG
+tar -C "$BUILDDIR"/scylla-package -xpf "$RELOC_PKG"
 cd "$BUILDDIR"/scylla-package
 
 mv scylla-tools/debian debian
 PKG_NAME=$(dpkg-parsechangelog --show-field Source)
-# XXX: Drop revision number from version string.
-#      Since it always '1', this should be okay for now.
 PKG_VERSION=$(dpkg-parsechangelog --show-field Version |sed -e 's/-1$//')
-ln -fv $RELOC_PKG ../"$PKG_NAME"_"$PKG_VERSION".orig.tar.gz
+ln -fv "$RELOC_PKG" ../"$PKG_NAME"_"$PKG_VERSION".orig.tar.gz
 debuild -rfakeroot -us -uc
