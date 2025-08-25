6293fabeb7541f915a891fce1c64bb8d0832a487
Dmitry Kropachev <dmitry.kropachev@gmail.com>
2025-08-25 11:09:29 -0400

    1

--- integration-tests/user-profile-cqlstress-example.sh
+++ integration-tests/user-profile-cqlstress-example.sh
@@ -3,8 +3,11 @@
 set -xe
 
 IP=$1
+DRIVER_TYPE=${2:-3x}
+[[ "$DRIVER_TYPE" == "3x" ]] && DRIVER_TYPE=native
 
 cassandra-stress user "ops(insert=1)" "profile=$PWD/examples/cqlstress-example.yaml" no-warmup n=10000 cl=QUORUM \
     -errors fail-fast \
-    -node "$IP" \
+    -node "$IP" datacenter=datacenter1 \
+    -mode cql3 "${DRIVER_TYPE}" \
     -rate threads=4 || exit 1
